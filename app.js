36c9ea679e163555710d28a2d31a0d04caf82bb8
vikram110703 <sainivks4321@gmail.com>
2023-10-03 00:51:34 +0530

    frontend url added

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());//accept req.cookie;
 
 app.use(
     cors({
-      origin: "http://localhost:5173",
+       origin: [process.env.FRONTEND_URL],
       credentials: true,
     })
   );
